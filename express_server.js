dc4d8e07e18f3107275f332247eb61c89c106140
Sonofarobin <zanthrobinson@gmail.com>
2019-10-28 19:27:48 +0000

    views dir added.

--- express_server.js
+++ express_server.js
@@ -20,3 +20,8 @@ app.listen(PORT, () => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
