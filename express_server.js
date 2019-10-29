dc198ca4a9450abd19624cf2304701d64762a018
Sonofarobin <zanthrobinson@gmail.com>
2019-10-29 23:34:00 +0000

    Cookies clearing and also cookies is deleted to string undefined

--- express_server.js
+++ express_server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cookieParser = require('cookie-parser')
 const app = express();
 const PORT = 8080; // default port 8080
 
@@ -17,6 +18,7 @@ function generateRandomString() {
 }
 
 app.set('view engine', 'ejs');
+app.use(cookieParser())
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -39,16 +41,20 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase};
+  if (req.cookies.username) {
+    templateVars.username = req.cookies.username
+  }
+  console.log(`logging cookie`, req.cookies);
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  res.render("urls_new", templateVars = {username: req.cookies.username});
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies.username };
   console.log([req.params]);
   res.render("urls_show", templateVars);
 });
@@ -78,3 +84,12 @@ app.post(`/urls/:shortURL`, (req, res) => {
   res.redirect(`/urls`);
 });
 
+app.post(`/login`, (req, res) => {
+  res.cookie(`username`, req.body[`username`]);
+  res.redirect(`/urls`);
+});
+
+app.post(`/logout`, (req, res) => {
+  res.clearCookie(`username`);
+  res.redirect(`/urls`);
+});
