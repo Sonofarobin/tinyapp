f3a54c4350b35b25d59a7b76ee204c3138f3df78
Sonofarobin <zanthrobinson@gmail.com>
2019-10-29 17:04:01 +0000

    Day 1 finished

--- express_server.js
+++ express_server.js
@@ -2,6 +2,20 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+function makeid(length) {
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+function generateRandomString() {
+  return makeid(6);
+}
+
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
@@ -17,6 +31,9 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -26,8 +43,25 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   console.log([req.params]);
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  let id = generateRandomString()
+  console.log(req.body);  // Log the POST request body to the console
+  urlDatabase[id] = req.body.longURL;  
+  res.redirect(`/urls/${id}`)
+});
+
+app.get(`/u/:shortURL`, (req, res) => {
+  const shortcut = req.params.shortURL;
+  res.redirect(`http://${urlDatabase[shortcut]}`)
+  console.log(shortcut + urlDatabase[shortcut]);
 });
